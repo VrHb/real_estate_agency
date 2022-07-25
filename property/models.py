8c01de7d7f821c36b84e007ec4d5b98ab1268808
Victor Abdukarimov <viredmi@gmail.com>
2022-07-25 18:32:44 +0300

    Added field Flat.owner_pure_phone

--- property/models.py
+++ property/models.py
@@ -2,51 +2,63 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+from phonenumber_field.modelfields import PhoneNumberField
+
+
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
+    owner_pure_number = PhoneNumberField(
+        'Нормализованный номер владельца',
+        blank=True
+    )
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     new_building = models.BooleanField('Новостройка', default=None, null=True) 
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
-        db_index=True)
-
+        db_index=True
+    )
     description = models.TextField('Текст объявления', blank=True)
     price = models.IntegerField('Цена квартиры', db_index=True)
-
     town = models.CharField(
         'Город, где находится квартира',
         max_length=50,
-        db_index=True)
+        db_index=True
+    )
     town_district = models.CharField(
         'Район города, где находится квартира',
         max_length=50,
         blank=True,
-        help_text='Чертаново Южное')
+        help_text='Чертаново Южное'
+    )
     address = models.TextField(
         'Адрес квартиры',
-        help_text='ул. Подольских курсантов д.5 кв.4')
+        help_text='ул. Подольских курсантов д.5 кв.4'
+    )
     floor = models.CharField(
         'Этаж',
         max_length=3,
-        help_text='Первый этаж, последний этаж, пятый этаж')
-
+        help_text='Первый этаж, последний этаж, пятый этаж'
+    )
     rooms_number = models.IntegerField(
         'Количество комнат в квартире',
-        db_index=True)
+        db_index=True
+    )
     living_area = models.IntegerField(
         'количество жилых кв.метров',
         null=True,
         blank=True,
-        db_index=True)
-
+        db_index=True
+    )
     has_balcony = models.NullBooleanField('Наличие балкона', db_index=True)
     active = models.BooleanField('Активно-ли объявление', db_index=True)
     construction_year = models.IntegerField(
         'Год постройки здания',
         null=True,
         blank=True,
-        db_index=True)
+        db_index=True
+    )
+    
     likes = models.ManyToManyField(
         User, 
         verbose_name='Кто лайкнул',
